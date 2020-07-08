533bb9e58a7edf0ee41e383cf1091af93aeb5bb1
신주현 <shinjhhp5@gmail.com>
2020-07-09 01:41:22 +0900

    post

--- DjangoInstaV1/urls.py
+++ DjangoInstaV1/urls.py
@@ -1,10 +1,19 @@
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django_pydenticon.views import image as pydenticon_image
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('identicon/image/<path:data>',
+         pydenticon_image, name='pydenticon_image'),
+    path('accounts/', include('accounts.urls')),
+    path('', include('instagram.urls')),
+    path('', login_required(TemplateView.as_view(
+        template_name='root.html')), name='root'),
 ]
 
 if settings.DEBUG:
---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # /accounts/login/ => settings.LOGIN_URL
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('password_change/', views.password_change, name='password_change'),
+    path('signup/', views.signup, name='signup'),
+    path('edit/', views.profile_edit, name='profile_edit'),
+]
